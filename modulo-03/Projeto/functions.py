293f1b29b975026dca428e5835fcf89758853441
Caiokl3i <caio.victor.santos12@gmail.com>
2025-09-18 10:31:21 -0400

    feat: adicionar arquivo de funções e modulariza

--- modulo-03/Projeto/functions.py
+++ modulo-03/Projeto/functions.py
@@ -1,27 +1,4 @@
-"""
-Projeto: Sistema Bancário (Versão 2)
 
-Com base na versão anterior do sistema bancário, adicione as seguintes funcionalidades complementares:
-
-1. Limite de Transações Diárias
-   - O sistema deve permitir no máximo 10 transações por dia (incluindo depósitos e saques).
-   - Caso o usuário tente realizar uma transação após atingir o limite, o sistema deve exibir uma mensagem 
-     informando que o número de transações diárias foi excedido.
-
-2. Controle de Saques (regras mantidas da versão 1)
-   - Continuam válidas as regras: máximo de 3 saques por dia e limite de R$ 500,00 por saque.
-
-3. Registro de Data e Hora
-   - Cada transação (depósito ou saque) deve ser registrada com a data e hora exata em que foi realizada.
-   - Essas informações devem aparecer no extrato ao lado de cada movimentação.
-
-4. Extrato Atualizado
-   - O extrato deve exibir, em ordem, todas as transações do dia, mostrando:
-        - Tipo da operação (Depósito ou Saque)
-        - Valor da operação
-        - Data e hora da operação
-   - Ao final, deve exibir o saldo atual da conta.
-"""
 
 def mostrar_menu():
     while True:
@@ -107,24 +84,3 @@ def consultar_extrato(depositos_extrato, saques_extrato, carteira):
     print(f'Total de saques: R${sum(saques_extrato):.2f}')
 
     print(f'\nSaldo atual: R${carteira:.2f}')
-
-depositos_extrato = []
-saques_extrato = []
-carteira = 0
-limite_saques = 3
-saque_diario = 0
-
-while True:
-    option = mostrar_menu()
-
-    match option:
-        case 1:
-            carteira, depositos_extrato = realizar_deposito(carteira, depositos_extrato)
-        case 2:
-            carteira, saques_extrato, saque_diario = realizar_saque(carteira, saques_extrato, saque_diario, limite_saques)
-            print(f'\nsaque diario {saque_diario}')
-        case 3:
-            consultar_extrato(depositos_extrato, saques_extrato, carteira)
-        case 0:
-            print('\nSaindo ...')
-            break
